2af1ec2d3823775d8a044680c2ae910f5a374227
Driss <dri-el-dri3@hotmail.com>
2023-11-17 15:41:48 +0100

    added exercises up to page 115

---
+++ chapter three/buffet_touple_loop.py
@@ -0,0 +1,9 @@
+simple_foods = ("fried potatoes", "potatoe mash", "baked potatoe", "potatoe salad")
+print("This buffet sells:")
+for simple_food in simple_foods:  
+    print(f"\n - {simple_food}")
+
+simple_foods = ("french fries", "Mashed potatoes", "baked potatoe", "potatoe salad")
+print("This buffet sells:")
+for simple_food in simple_foods:  
+    print(f"\n - {simple_food.title()}")
---
+++ chapter three/conditional_tests_case.py
@@ -0,0 +1,36 @@
+car = "honda"
+print("Is car == 'honda'? I predict True.")
+print(car == 'honda')
+
+car_years=1
+print("Is car age >= 0? I predict True")
+print(car_years>=0)
+print("Is car age <= 1? I predict True")
+print(car_years<=0)
+
+print("Is car == audi? I predict False")
+print(car == 'audi')
+
+car_keys="At home"
+print("Are car keys at home? I predict True")
+print(car_keys=="At home")
+
+car_keys="At home"
+print("Are car keys here? I predict False")
+print(car_keys=="At home")
+
+studying="from home" 
+print("Am I studying from home? I predict true")
+print(studying=="from home")
+
+location= "home"
+print("Am I in a submarine? I predict False")
+print (location == "submarine")
+
+location= "home"
+print("Am I in a plane? I predict False")
+print (location == "plane")
+
+species= "human"
+print("Am I a dog? I predict False")
+print (species == "dog")
