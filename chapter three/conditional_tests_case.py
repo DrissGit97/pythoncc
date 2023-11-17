ea49075f7a6bd9f0d17ce37fda083fa8a60f8858
Driss <dri-el-dri3@hotmail.com>
2023-11-17 15:55:52 +0100

    added exercises up to page 115 (including 5-2)

--- chapter three/conditional_tests_case.py
+++ chapter three/conditional_tests_case.py
@@ -34,3 +34,25 @@ print (location == "plane")
 species= "human"
 print("Am I a dog? I predict False")
 print (species == "dog")
+
+car= "audi"
+print("Is the car an Audi? I predict True")
+print(car=="Audi".lower())
+
+car= "Nissan" 
+country= "Japan"
+print("Is the car a nissan and from Japan? I predict True") 
+print(car=="nissan".title() and country=="Japan".title())
+
+car= "German_brand" 
+country= "Japan"
+print("Is the car a german_brand or from Japan? I predict True") 
+print(car=="German_brand".title() or country=="Japan".title())
+
+years=("2001","2002","2003")
+print("Is 2001 included in the list?")
+print("2001" in years)
+
+year="2004"
+if year not in years:
+    print(f'The year {year} is not included in the list')
