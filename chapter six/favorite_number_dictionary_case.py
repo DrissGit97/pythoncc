2319866ec545a5db85290ce8b2ce75895a167e62
Driss <dri-el-dri3@hotmail.com>
2023-11-18 15:53:29 +0100

    Added more exercises from chapter six

---
+++ chapter six/favorite_number_dictionary_case.py
@@ -0,0 +1,12 @@
+favorite_numbers = {
+    "person_A":"1",
+    "person_B":"2",
+    "person_C":"3",
+    "person_D":"4",
+    "person_E":"5",
+}
+print(f"person_A's favorite number is {favorite_numbers['person_A']}")
+print(f"person_B's favorite number is {favorite_numbers['person_B']}")
+print(f"person_C's favorite number is {favorite_numbers['person_C']}")
+print(f"person_D's favorite number is {favorite_numbers['person_D']}")
+print(f"person_E's favorite number is {favorite_numbers['person_E']}")
---
+++ chapter six/person_dictionary_case.py
@@ -0,0 +1,5 @@
+person = {'name':"person_name", 'last_name':"person_last_name",'age':"person_age",'city':"person_city"}
+print(person['name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
